ae9a86e0b44c457d1328bb7d70209a268afa405b
Ziad Alezzi <lucirieko@gmail.com>
2026-06-19 19:28:44 +0300

    Added currencies rate method

--- app/services/frankfurter_client.rb
+++ app/services/frankfurter_client.rb
@@ -1,6 +1,13 @@
 class FrankfurterClient
     def self.currencies
-        response = HTTParty.get("https://api.frankfurter.app/currencies")
+        response = HTTParty.get("https://api.frankfurter.dev/v2/currencies")
+        response.parsed_response
+    end
+
+    def self.rate(base, quote)
+        response = HTTParty.get("https://api.frankfurter.dev/v2/rates",
+        query: { base: base, quotes: quote }
+        )
         response.parsed_response
     end
 end
