214c614f14e30436c8da154d7cbc777d897fa2f5
Ziad Alezzi <lucirieko@gmail.com>
2026-06-20 10:13:20 +0300

    js controller fetches rates and automatically updates result to user input

--- app/javascript/controllers/converter_controller.js
+++ app/javascript/controllers/converter_controller.js
@@ -3,13 +3,20 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
     static targets = ["amount", "from", "to", "result"]
 
-    convert(event) {
+    async convert(event) {
         event.preventDefault()
 
         const amount = this.amountTarget.value
         const from = this.fromTarget.value
         const to = this.toTarget.value
 
-        this.resultTarget.textContent = `${amount} of ${from} = x ${to}`
+
+        const url = `https://api.frankfurter.dev/v2/rates?base=${from}&quotes=${to}`
+        const response = await fetch(url)
+        const data = await response.json()
+
+        const rate = data[0]["rate"]
+        const converted_price = (amount * rate).toFixed(3)
+        this.resultTarget.textContent = converted_price
     }
 }
